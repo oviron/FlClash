2d5df8109863348d7f17e78e7214d7c08a29208f
Dmitry Avdonkin <awdonkin@gmail.com>
2026-05-31 18:44:39 +0300

    refactor: extract input add dialog

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -10,8 +10,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'effect.dart';
+import 'input_add_dialog.dart';
 import 'list.dart';
 import 'theme.dart';
+
+export 'input_add_dialog.dart';
 
 class OptionsDialog<T> extends StatelessWidget {
   final String title;
@@ -693,127 +696,6 @@ class _MapInputPageState extends ConsumerState<MapInputPage> {
                 },
                 onReorder: _handleReorder,
               ),
-      ),
-    );
-  }
-}
-
-class AddDialog extends StatefulWidget {
-  final String title;
-  final Field? keyField;
-  final Field valueField;
-
-  const AddDialog({
-    super.key,
-    required this.title,
-    this.keyField,
-    required this.valueField,
-  });
-
-  @override
-  State<AddDialog> createState() => _AddDialogState();
-}
-
-class _AddDialogState extends State<AddDialog> {
-  TextEditingController? _keyController;
-  late TextEditingController _valueController;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  Field? get keyField => widget.keyField;
-
-  Field get valueField => widget.valueField;
-
-  @override
-  void initState() {
-    super.initState();
-    if (keyField != null) {
-      _keyController = TextEditingController(text: keyField!.value);
-    }
-    _valueController = TextEditingController(text: valueField.value);
-  }
-
-  void _submit() {
-    if (!_formKey.currentState!.validate()) return;
-    if (keyField != null) {
-      Navigator.of(context).pop<MapEntry<String, String>>(
-        MapEntry(_keyController!.text, _valueController.text),
-      );
-    } else {
-      Navigator.of(context).pop<String>(_valueController.text);
-    }
-  }
-
-  @override
-  void dispose() {
-    _keyController?.dispose();
-    _valueController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return CommonDialog(
-      title: widget.title,
-      actions: [
-        TextButton(onPressed: _submit, child: Text(appLocalizations.confirm)),
-      ],
-      child: Form(
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        key: _formKey,
-        child: Wrap(
-          runSpacing: 16,
-          children: [
-            if (keyField != null)
-              TextFormField(
-                maxLines: 3,
-                minLines: 1,
-                controller: _keyController,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: keyField!.label,
-                ),
-                validator: (String? value) {
-                  String? res;
-                  if (keyField!.validator != null) {
-                    res = keyField!.validator!(value);
-                  }
-                  if (res != null) {
-                    return res;
-                  }
-                  if (value == null || value.isEmpty) {
-                    return appLocalizations.emptyTip(appLocalizations.key);
-                  }
-                  return null;
-                },
-              ),
-            TextFormField(
-              maxLines: 3,
-              minLines: 1,
-              keyboardType: TextInputType.text,
-              controller: _valueController,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: valueField.label,
-              ),
-              onFieldSubmitted: (_) {
-                _submit();
-              },
-              validator: (String? value) {
-                String? res;
-                if (valueField.validator != null) {
-                  res = valueField.validator!(value);
-                }
-                if (res != null) {
-                  return res;
-                }
-                if (value == null || value.isEmpty) {
-                  return appLocalizations.emptyTip(appLocalizations.value);
-                }
-                return null;
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
